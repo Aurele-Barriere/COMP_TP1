5f9904d725105479e4668a7713e4f13190e4f670
Antonin Garret <16013984@e105m07.istic.univ-rennes1.fr>
2016-10-07 09:12:47 +0200

    all rules, no return value for now

--- FirstVersion/firstversion.ml
+++ FirstVersion/firstversion.ml
@@ -10,7 +10,7 @@ let int_of_char c = Char.code c - Char.code '0'*)
 
 (* First, we define the tokens (lexical units) *)
 
-type token = Point | Semicolon | Comma | LeftBracket | RightBracket | S of string
+type token = Point | Semicolon | Comma | LeftBracket | RightBracket | S of string | Quote
 
 (* Then we define the lexer, using stream parsers with only right recursion *)
 
@@ -27,14 +27,42 @@ and token = parser
   | [< ' (';') >] -> Semicolon
   | [< ' ('<') >] -> LeftBracket
   | [< ' ('>') >] -> RightBracket
+  | [< ' ('"') >] -> Quote
   | [< 'c when (c>= '1' && c <= '9') || (c>= 'a' && c<='z'); s = token_string (string_of_char c) >] -> S s
 and token_string s_read = parser
 | [< 'c when (c>= '1' && c <= '9') || (c>= 'a' && c<='z'); s = token_string((String.make 1 c) ^ s_read) >] -> s
 | [< >] -> s_read
 
-(* Next, we define a type to represent abstract syntax trees: *)
 
-(* type expr = Num of int | Add of expr * expr | Sub of expr * expr | Mul of expr * expr *)
+let rec parse_document = parser
+| [< s1 = parse_subject; 'Point; s2 = parse_document >] -> ""
+| [< >] ->""
+
+and parse_subject = parser
+| [< 'LeftBracket; 'S(id); 'RightBracket; p1 = parse_predicate_list >] ->""
+
+and parse_predicate_list = parser
+| [< p1 = parse_predicate; p2 = parse_predicate_list_aux >] ->""
+
+and parse_predicate_list_aux = parser
+| [< 'Semicolon; p1 = parse_predicate_list >] ->""
+| [< >] ->""
+
+and parse_predicate = parser
+| [< 'LeftBracket; 'S(id); 'RightBracket; o1 = parse_object_list >] ->""
+
+and parse_object_list = parser
+| [< o1 = parse_object; o2 = parse_object_list_aux >] ->""
+
+and parse_object_list_aux = parser
+| [< 'Comma; o1 = parse_object_list >] ->""
+| [< >] ->""
+
+and parse_object = parser
+| [< 'LeftBracket; 'S(id); 'RightBracket >] ->""
+| [< 'Quote; 'S(id); 'Quote >] ->""
+
+(* type expr = Num of int | Add of expr * expr | Sub of expr * expr | Mul of expr * expr   => NO AST for now*)
 
 (* The recursive descent parser consists of three mutually-recursive functions: *)
 (* 
