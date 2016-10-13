e7dacccef95140d6cfc2a84337a8225ace7a4a66
Aurele-Barriere <aurele.barriere@gmail.com>
2016-10-13 11:44:58 +0200

    printing walker

--- FirstVersion/walker.ml
+++ FirstVersion/walker.ml
@@ -3,8 +3,31 @@
 
 open Astparser
 
+let obj_to_string (o:obj)=
+  match o with
+  | I(e) -> " | | <" ^ e ^ ">"
+  | S(t) -> " | | \"" ^ t ^ "\""
+
+let predicate_to_string ((e,ol):predicate) =
+  " | " ^ e ^ "\n" ^
+    (List.fold_right
+       (fun o l -> obj_to_string(o) ^ "\n" ^ l)
+       ol "")
+
+let subject_to_string ((e,pl):subject) =
+  e ^ "\n" ^
+    (List.fold_right
+       (fun p l -> predicate_to_string(p) ^ "\n" ^ l)
+       pl "")
+
+let document_to_string (sl : document) =
+  (List.fold_right
+     (fun s l -> subject_to_string(s) ^ "\n" ^ l)
+     sl "")
+       
 let print_ast (d : document) =
-  Printf.printf("TO DO : print an AST\n")
+  Printf.printf "%s" (document_to_string d)
+
    
 let produce_ntriple (d : document) =
   "" (* TO DO *)
